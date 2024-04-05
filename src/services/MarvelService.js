eaa9eecee16c1a858a0833356046a3409f65b99b
Saimon <saimon22011@gmail.com>
2024-04-05 17:57:40 +0400

    feat: added logic for loading more characters for the button loaded more

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,6 +1,7 @@
 class MarvelService{
     _apiBase = 'https://gateway.marvel.com:443/v1/public/';
     _apiKey = 'apikey=805df13e53662d329f8a1db9f387a41d';
+    _baseOffset = 210;
     getResource = async (url) =>{
         let res = await fetch(url);
 
@@ -11,8 +12,8 @@ class MarvelService{
         return await res.json();
     }
 
-    getAllCharacters = async () => {
-        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+    getAllCharacters = async (offset = this._baseOffset) => {
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=${offset}&${this._apiKey}`);
         return res.data.results.map(this._transformCharacter)
     }
 
