fcc9936dc5c18f61c7bf253f899e4c10ca417c94
Saimon <saimon22011@gmail.com>
2024-04-05 17:18:59 +0400

    feat: added Error Boundary for components in case of an error

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -3,6 +3,7 @@ import AppHeader from "../appHeader/AppHeader";
 import RandomChar from "../randomChar/RandomChar";
 import CharList from "../charList/CharList";
 import CharInfo from "../charInfo/CharInfo";
+import ErrorBoundary from "../errorBoundary/ErrorBoundary";
 
 import decoration from '../../resources/img/vision.png';
 
@@ -23,10 +24,16 @@ class App extends Component{
             <div className="app">
                 <AppHeader/>
                 <main>
-                    <RandomChar/>
+                    <ErrorBoundary>
+                        <RandomChar/>
+                    </ErrorBoundary>
                     <div className="char__content">
-                        <CharList onCharSelected = {this.onCharSelected}/>
-                        <CharInfo charId={this.state.selectedChar}/>
+                        <ErrorBoundary>
+                            <CharList onCharSelected = {this.onCharSelected}/>
+                        </ErrorBoundary>
+                        <ErrorBoundary>
+                            <CharInfo charId={this.state.selectedChar}/>
+                        </ErrorBoundary>
                     </div>
                     <img className="bg-decoration" src={decoration} alt="vision"/>
                 </main>
--- src/components/charInfo/CharInfo.js
+++ src/components/charInfo/CharInfo.js
@@ -26,6 +26,7 @@ class CharInfo extends Component{
         }
     }
 
+
     updateChar = () =>{
         const {charId} = this.props;
         if(!charId){
