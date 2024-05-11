a3c4e0b3321e7f961481f1bb883e4d645bd12f38
Saimon <saimon22011@gmail.com>
2024-05-11 13:48:32 +0400

    upd:updated react-router-dom v5>v6

--- src/components/appHeader/AppHeader.js
+++ src/components/appHeader/AppHeader.js
@@ -11,9 +11,14 @@ const AppHeader = () => {
             </h1>
             <nav className="app__menu">
                 <ul>
-                    <li><NavLink exact activeStyle={{'color':'#9f0013'}} to="/">Characters</NavLink></li>
+                    <li><NavLink
+                        end
+                        style={({isActive}) => ({color: isActive ? '#9f0013' : 'inherit'})}
+                        to="/">Characters</NavLink></li>
                     /
-                    <li><NavLink exact activeStyle={{'color':'#9f0013'}} to="/comics">Comics</NavLink></li>
+                    <li><NavLink end
+                                 style={({isActive}) => ({color: isActive ? '#9f0013' : 'inherit'})}
+                                 to="/comics">Comics</NavLink></li>
                 </ul>
             </nav>
         </header>
